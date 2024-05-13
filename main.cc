6f361b7b548179098f488b300128abcdf18f4e77
connortbot <loiconnor8@gmail.com>
2024-05-13 19:56:54 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -35,19 +35,18 @@ void load_test() {
 }
 
 int main(int argc, char* argv[]) {
-    load_test();
-    // Config config = parseArguments(argc, argv);
+    Config config = parseArguments(argc, argv);
     
-    // RenderData render_data;
-    // const auto aspect_ratio = static_cast<float>(config.image_width) / config.image_height;
-    // setRenderData(render_data, aspect_ratio, config.image_width, config.samples_per_pixel, config.max_depth);
-    // std::string filePath = config.inputFile;
-    // RTCDevice device = initializeDevice();
-    // CSRParser parser;
-    // auto scene_ptr = parser.parseCSR(filePath, device);
-    // scene_ptr->commitScene();
-    // rtcReleaseDevice(device);
+    RenderData render_data;
+    const auto aspect_ratio = static_cast<float>(config.image_width) / config.image_height;
+    setRenderData(render_data, aspect_ratio, config.image_width, config.samples_per_pixel, config.max_depth);
+    std::string filePath = config.inputFile;
+    RTCDevice device = initializeDevice();
+    CSRParser parser;
+    auto scene_ptr = parser.parseCSR(filePath, device);
+    scene_ptr->commitScene();
+    rtcReleaseDevice(device);
 
-    // output(render_data, scene_ptr->cam, scene_ptr, config);
+    output(render_data, scene_ptr->cam, scene_ptr, config);
 }
 
