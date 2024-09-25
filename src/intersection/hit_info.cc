470b4ba27fe7e123abe771c88959192085420392
Pranav Bedi <70117486+pranavbedi@users.noreply.github.com>
2024-09-25 16:13:41 -0400

    Update hit_info.cc

--- src/intersection/hit_info.cc
+++ src/intersection/hit_info.cc
@@ -4,3 +4,6 @@ void HitInfo::set_face_normal(const ray& r, const vec3& outward_normal) {
     front_face = dot(r.direction(), outward_normal) < 0;
     normal = front_face ? outward_normal : -outward_normal;
 }
+
+
+#TESTTESTTESTPLEASEREMOVE
